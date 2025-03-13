13527087371b63f7dc9370e560cd9b588fadd5ff
Seva Paramonov <57590281+vsevolod-paramonov@users.noreply.github.com>
2025-03-13 22:55:29 +0300

    Fixed decoder.py

--- models/seq2seq/decoder.py
+++ models/seq2seq/decoder.py
@@ -7,7 +7,6 @@ class BahdanauAttention(nn.Module):
     def __init__(self, hidden_size, num_layers):
         super(BahdanauAttention, self).__init__()
         self.Wa = nn.Linear(hidden_size * num_layers, hidden_size)
-
         self.Ua = nn.Linear(hidden_size, hidden_size)
         self.Va = nn.Linear(hidden_size, 1)
 
@@ -23,51 +22,40 @@ class BahdanauAttention(nn.Module):
 class AttnDecoderRNN(nn.Module):
     def __init__(self, dataset, hidden_size, num_layers, multiply, dropout_p, device=None):
         super(AttnDecoderRNN, self).__init__()
-        self.device = device 
-        self.dataset = dataset
+        self.device = device
+        self.en = dataset
+
         self.embedding = nn.Embedding(dataset.vocab_size, hidden_size)
-        self.attention = BahdanauAttention(hidden_size, num_layers * (2*(multiply==True) + 1*(multiply==False)))
-        self.gru = nn.GRU(hidden_size * 2, hidden_size, num_layers=num_layers * (2*(multiply==True) + 1*(multiply==False)), batch_first=True)
+        num_layers_adj = num_layers * (2 if multiply else 1)
+
+        self.attention = BahdanauAttention(hidden_size, num_layers_adj)
+        self.gru = nn.GRU(hidden_size * 2, hidden_size, num_layers=num_layers_adj, batch_first=True)
+
         self.out = nn.Linear(hidden_size, dataset.vocab_size)
         self.dropout = nn.Dropout(dropout_p)
 
-    def forward(self, encoder_outputs, encoder_hidden, target_tensor=None):
-        batch_size = encoder_outputs.size(0)
-        decoder_input = torch.empty(batch_size, 1, dtype=torch.long, device=self.device).fill_(self.dataset.bos_id)
+    def forward(self, encoder_outputs, encoder_hidden, decoder_input):
         decoder_hidden = encoder_hidden
-        decoder_outputs = []
-        attentions = []
 
-        assert target_tensor is not None, 'Please, insert target_tensor for faster inference'
+        decoder_outputs, attentions = [], []
 
-        for _ in range(target_tensor.shape[1]):
-            decoder_output, decoder_hidden, attn_weights = self.forward_step(
-                decoder_input, decoder_hidden, encoder_outputs
-            )
+        for i in range(decoder_input.shape[1]):
+            decoder_output, decoder_hidden, attn_weights = self.forward_step(decoder_input[:, i:i + 1], decoder_hidden, encoder_outputs)
             decoder_outputs.append(decoder_output)
             attentions.append(attn_weights)
-    
-            _, topi = decoder_output.topk(1)
-            decoder_input = topi.squeeze(-1).detach() 
 
         decoder_outputs = torch.cat(decoder_outputs, dim=1)
-        decoder_outputs = F.log_softmax(decoder_outputs, dim=-1)
         attentions = torch.cat(attentions, dim=1)
 
-        return decoder_outputs, decoder_hidden, attentions
-
+        return F.log_softmax(decoder_outputs, dim=-1), decoder_hidden, attentions
 
     def forward_step(self, input, hidden, encoder_outputs):
-        input = input.to(self.device)
-
-        embedded =  self.dropout(self.embedding(input))
+        embedded = self.dropout(self.embedding(input))
 
         query = hidden.permute(1, 0, 2).reshape(hidden.shape[1], 1, -1)
-
         context, attn_weights = self.attention(query, encoder_outputs)
 
         input_gru = torch.cat((embedded, context), dim=2)
-
 
         output, hidden = self.gru(input_gru, hidden)
         output = self.out(output)
