d6c6bf42dab6456c1a438049131f9bfe9b075f5b
Seva Paramonov <57590281+vsevolod-paramonov@users.noreply.github.com>
2025-03-13 22:56:00 +0300

    Fixed translator.py

--- models/seq2seq/translator.py
+++ models/seq2seq/translator.py
@@ -1,8 +1,6 @@
 import torch.nn as nn
 import torch
 
-import sys
-
 class Translator(nn.Module):
     def __init__(self, encoder, decoder, config, device):
         super(Translator, self).__init__()
@@ -12,16 +10,21 @@ class Translator(nn.Module):
         self.encoder = encoder
         self.decoder = decoder
 
+        self.de = encoder.de
+        self.en = decoder.en
+
     def forward(self, source, target=None):
         encoder_outputs, encoder_hidden = self.encoder(source)
 
-        # sys.stdout.write(f'\n{encoder_outputs.shape}, {encoder_hidden.shape}\n')
-
-        # sys.stdout.write(f'\n{self.decoder}')
-
-        outputs, _, _ = self.decoder(
-            encoder_outputs, encoder_hidden, target_tensor=target
-        )
+        if target is not None:
+            outputs, _, _ = self.decoder(encoder_outputs, encoder_hidden, target)
+        else:
+            outputs, _, _ = self.decoder(encoder_outputs, encoder_hidden, torch.full((source.size(0), 1), self.de.bos_id, dtype=torch.long, device=self.device))
 
         return outputs
-    
+
+    def encode(self, input):
+        return self.encoder(input)
+
+    def decode(self, encoder_outputs, encoder_hidden, decoder_input):
+        return self.decoder(encoder_outputs, encoder_hidden, decoder_input)
