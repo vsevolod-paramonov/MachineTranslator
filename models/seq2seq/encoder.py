d31b418fa7172569b34d10d2db8b07750930be09
Seva Paramonov <57590281+vsevolod-paramonov@users.noreply.github.com>
2025-03-13 22:56:32 +0300

    Fixed encoder.py

--- models/seq2seq/encoder.py
+++ models/seq2seq/encoder.py
@@ -1,22 +1,23 @@
 import torch.nn as nn
 
+
 class EncoderRNN(nn.Module):
     def __init__(self, dataset, hidden_size, num_layers, dropout_p, bidirectional):
         super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
-        self.dataset = dataset
+        self.de = dataset
 
         self.embedding = nn.Embedding(dataset.vocab_size, hidden_size)
         self.gru = nn.GRU(hidden_size, hidden_size, num_layers=num_layers, bidirectional=bidirectional, batch_first=True)
         self.dropout = nn.Dropout(dropout_p)
 
-        self.fc = nn.Linear(hidden_size * 2, hidden_size)
+        self.fc = nn.Linear(hidden_size * 2, hidden_size) if bidirectional else nn.Identity()
 
     def forward(self, input):
         embedded = self.dropout(self.embedding(input))
         output, hidden = self.gru(embedded)
 
-        if self.gru.bidirectional: ### For dimension matching
+        if isinstance(self.fc, nn.Linear):
             output = self.fc(output)
 
-        return output, hidden ### Get only last state
+        return output, hidden
